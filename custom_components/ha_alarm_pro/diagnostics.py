2001cd35e2e5841baab37890d5bfbda87bdbe5f8
TimeLance89 <80360241+TimeLance89@users.noreply.github.com>
2025-11-10 07:57:09 +0100

    Del

--- custom_components/ha_alarm_pro/diagnostics.py
+++
@@ -1,8 +0,0 @@
-
-from __future__ import annotations
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry):
-    data = {**entry.data, **entry.options}
-    # Hide secrets if any
-    return {"options": data}
